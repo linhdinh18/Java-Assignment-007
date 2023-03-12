afe17cbba8df8286c2bc4e821e10c3e3dda91920
Linh Dinh <nhlinh1803@gmail.com>
2023-03-11 23:08:37 -0800

    Completed Java Assignment 007

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,37 @@
+/**
+ * @author Trevor Hartman
+ * @author Linh Dinh
+ *
+ * @since version 1.0
+ */
 import java.util.Scanner;
 
 public class MyMath {
 
-    public static void main(String[] args) {
+    public static void gcf() {
+        int a, b;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = s.nextInt();
+        System.out.println("Enter b: ");
+        b = s.nextInt();
+        int c = 0;
+        while (b != 0) {
+            if (a > b){
+                c = b;
+                b = a % b;
+                a = c;
+            } else {
+                b %= a;
+            }
+        }
+        System.out.println("The GCF of a and b is: " + c);
     }
+
+
+
+    public static void main(String[] args) {
+        gcf();
+    }
+
 }
